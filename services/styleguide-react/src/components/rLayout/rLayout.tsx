8a17f9ca617ade7c628ca02f576348c57a984e29
Darius Kruythoff <darius@kruythoff.net>
2026-07-01 08:32:20 +0200

    rLayout: Don't lose the c-kayout class

--- services/styleguide-react/src/components/rLayout/rLayout.tsx
+++ services/styleguide-react/src/components/rLayout/rLayout.tsx
@@ -2,14 +2,14 @@ import { RLayoutProps } from "./rLayout.types"
 
 export const RLayout = ({
   children,
-  className = "c-layout",
+  className,
   header,
   aside,
   "data-testid": dataTestid = "r-layout",
   ...props
 }: RLayoutProps) => {
   return (
-    <div className={className} data-testid={dataTestid} {...props}>
+    <div className={`c-layout${className ? ` ${className}` : ''}`} data-testid={dataTestid} {...props}>
       {header && <header>{header}</header>}
       {aside && <aside>{aside}</aside>}
       {children && <main>{children}</main>}
