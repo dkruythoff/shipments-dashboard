253f284646ff03928012c16bb9fef81bce46213e
Darius Kruythoff <darius@kruythoff.net>
2026-07-01 08:12:37 +0200

    React: RNavigation: null for conditional rendering

--- services/styleguide-react/src/components/rNavigation/rNavigation.tsx
+++ services/styleguide-react/src/components/rNavigation/rNavigation.tsx
@@ -1,22 +1,22 @@
 import { RNavigationProps } from "./rNavigation.types";
 
 export const RNavigation = ({ nodes }: RNavigationProps) => {
-  return (
-    !nodes?.length ? undefined : <ul className="c-navigation" data-testid="r-navigation">
-      {
-        nodes.map((node, nodeIndex) => {
-          return <li
-            key={node?.key || `navigation-item-${nodeIndex}`}
-            className={`c-navigation__item${node.active ? ' is-active' : ''}`}
-          >
-            {
-              node.href
-                ? <a href={node.href}>{node.label}</a>
-                : <button onClick={node.action}>{node.label}</button>
-            }
-          </li>
-        })
-      }
-    </ul>
-  );
+  if (!nodes?.length) return null
+
+  return <ul className="c-navigation" data-testid="r-navigation">
+    {
+      nodes.map((node, nodeIndex) => {
+        return <li
+          key={node?.key || `navigation-item-${nodeIndex}`}
+          className={`c-navigation__item${node.active ? ' is-active' : ''}`}
+        >
+          {
+            node.href
+              ? <a href={node.href}>{node.label}</a>
+              : <button onClick={node.action}>{node.label}</button>
+          }
+        </li>
+      })
+    }
+  </ul>
 };
